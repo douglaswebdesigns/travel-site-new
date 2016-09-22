0125921b5ffd263fcf2e7806831be5bba158ca75
douglaswebdesigns <paul@douglaswebdesigns.com>
2016-09-22 12:40:36 -0400

    new folder includes travel-site project files

---
+++ app/assets/scripts/App.js
@@ -0,0 +1,11 @@
+var $ = require ('jquery');
+var Person = require('./modules/Person');
+
+alert("ABC 4321");
+
+var john = new Person("John Doe", "Blue");
+john.greet();
+
+var jane = new Person("Jane Smith", "Green");
+jane.greet();
+
